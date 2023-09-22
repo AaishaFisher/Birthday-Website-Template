416825bb0a15d42a4f38404acb1ecc9b1adce97d
Aai'sha Fisher <aaishafisher484@gmail.com>
2023-09-23 00:55:28 +0200

    Major design part 1 complete

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ window.addEventListener('scroll', function(){
     mountains_behind.style.top = value * 0.5 + 'px';
     mountains_front.style.top = value * 0 + 'px';
     text.style.marginRight = value * 4 + 'px';
-    text.style.marginTop = value * 1.5 + 'px';
+    text.style.marginTop = value * 1.35 + 'px';
     btn.style.marginTop = value * 1.5 + 'px';
     header.style.top = value * 0.5 + 'px';
 }
